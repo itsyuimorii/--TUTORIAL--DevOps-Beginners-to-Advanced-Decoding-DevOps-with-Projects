bf1c8822c70c08a89b7fab43ae4b1970266702e4
YUIMORII <yuimoriistudio@gmail.com>
2022-12-25 18:13:08 -0600

    Update

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-# ⛺️ Bootcamp ブートキャンプ 2023 => Node.js, Express, MangoDB 
-
-## 🚀 Section 01: Welcome
-
-**[⬆ back to top](#table-of-contents)**
-
-> SYNCHRONOUS=>blocking
-
-```javascript
 const fs = require("fs");
 
 //Blocking code executed synchronously
@@ -16,12 +7,3 @@ const textOutput = `This is: ${textInput}.\n Created on ${Date.now()}`;
 
 fs.writeFileSync("./txt/output.txt", textOutput);
 console.log("File written to /txt/output.txt");
-```
-
-> ASYNCHRONOUS => non-blocking
-
-```javascript
-//non-blocking code executed
-fs.readFileSync("./txt/input.txt", "utf-8
-
-```
