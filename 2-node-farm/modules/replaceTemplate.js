45b6064fec7272c01b75412eca654e7b3b0cb57b
YUIMORII <yuimoriistudio@gmail.com>
2023-03-05 13:18:09 -0600

    Update replaceTemplate.js

--- 2-node-farm/modules/replaceTemplate.js
+++ 2-node-farm/modules/replaceTemplate.js
@@ -1,5 +1,6 @@
 module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%FROM%}/g, product.from);
