34efce41ed5b0313e364b8c74613f506ac3c4598
YUIMORII <yuimoriistudio@gmail.com>
2023-03-04 22:41:21 -0600

    update tempalte

--- 2-node-farm/index.js
+++ 2-node-farm/index.js
@@ -1,11 +1,35 @@
 const fs = require("fs");
-//Load http module
 const http = require("http");
 const url = require("url");
-
 const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-// const slugify = require('slugify');
+
+/////////////////////////////////
+// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written 😁');
+//       })
+//     });
+//   });
+// });
+// console.log('Will read file!');
 
 /////////////////////////////////
 // SERVER
@@ -21,57 +45,42 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-// 如果请求访问的是 index.html 文件,那么我们就要读取 index.html 文件的内容
-// 读取文件的内容,使用的方式 是 fs 模块
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
-//创建服务器对象
 const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  // const pathName = req.url;
-  const { query, pathName } = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
+
   // Overview page
-  // 根据路径地址,读取文件内容,显示在页面中
-  if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    // This function is supposed to replace all placeholders in template argument being passed.
-
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join("");
-    //console.log(cardsHtml);
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-    res.end(output);
-    //res.end(tempOverview);
-    //res.end("this is the overview page👀");
-
-    // Product page
-  } else if (pathName === "/product") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
 
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
-    //res.end("this is the product page🥗");
-
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
-
-    // 如果是读取成功,向页面输出,读取的文件内容
-    // 也就是 HTML文件的代码内容
-    // 代码内容,实际上是通过 node.js 搭建的服务器,交给浏览器来执行html文件的程序内容
     res.end(data);
 
     // Not found
@@ -85,5 +94,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port http://127.0.0.1:8000");
+  console.log("Listening to requests on port 8000");
 });
