391f52fd99a50caa13756deb9ca669bf3057fcae
YUIMORII <yuimoriistudio@gmail.com>
2023-03-07 14:25:44 -0600

    callback hell

--- 4-asynchronous-JS/index.js
+++ 4-asynchronous-JS/index.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+//callback hell
 fs.readFile(`"${__dirname}/dog.txt,`, (err, data) => {
   console.log(`Breed:${data}`);
 
   superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .get('https://dog.ceo/api/breed/hound/images/random')
     .end((err, res) => {
-      console.log(res.body);
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      //write dog image file
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('random dog image saved to file');
+      });
     });
 });
