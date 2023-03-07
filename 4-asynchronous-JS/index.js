b50b0ae21329d149db816cd53ec84dbe9f850a3c
YUIMORII <yuimoriistudio@gmail.com>
2023-03-07 14:56:31 -0600

    Update index.js

--- 4-asynchronous-JS/index.js
+++ 4-asynchronous-JS/index.js
@@ -22,21 +22,18 @@ const writeFilePro = (file) => {
 
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
-    console.log(`Breed:${data}`);
-    return superagent.get('https://dog.ceo/api/breed/${data}/images/random');
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
   })
-
   .then((res) => {
     console.log(res.body.message);
-
     return writeFilePro('dog-img.txt', res.body.message);
-    // fs.writeFile('dog-img.txt', res.body.message, (err) => {
-    //   if (err) return console.log(err.message);
-    //   console.log('Random dog image saved to file');
-    // });
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
   })
   .catch((err) => {
-    console.log(err.message);
+    console.log(err);
   });
 
 //callback hell😤
