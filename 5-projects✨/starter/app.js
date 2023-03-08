d889a57c770e5a15d6cfc9c6594d678ee729e25c
YUIMORII <yuimoriistudio@gmail.com>
2023-03-08 12:09:55 -0600

    Update app.js

--- 5-projects✨/starter/app.js
+++ 5-projects✨/starter/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+
+//Get an instance (object) of the server
 const app = express();
 
+app.get('/', (req, res) => {
+  res.end('welcome!');
+});
+
+//Start the server
 const port = 3000;
-app.listen(port, () => {
-  console.log('Server is listening on http://localhost:3000');
+app.listen(port, (error) => {
+  if (!error)
+    console.log(
+      `Server is Successfully Running, and App is listening on port ${port}...`
+    );
+  else console.log("Error occurred, server can't start", error);
 });
