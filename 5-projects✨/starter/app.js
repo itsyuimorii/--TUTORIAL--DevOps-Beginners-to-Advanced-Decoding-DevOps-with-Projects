eab6456cc22ed5d224cb9969004f21f0a632290e
YUIMORII <yuimoriistudio@gmail.com>
2023-03-08 13:16:47 -0600

    router

--- 5-projects✨/starter/app.js
+++ 5-projects✨/starter/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 //Get an instance (object) of the server
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'itsyuimoriiTours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'itsyuimoriiTours' });
+// });
+
+app.get('/api/v1/tours', (req, res) => {});
 
 //Start the server
 const port = 3000;
