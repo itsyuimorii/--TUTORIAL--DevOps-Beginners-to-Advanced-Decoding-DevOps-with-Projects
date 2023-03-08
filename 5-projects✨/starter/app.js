9405ae4ffbf0976cadfa5187ce47db4dbd3c2aa3
YUIMORII <yuimoriistudio@gmail.com>
2023-03-08 12:42:50 -0600

    setup app.js

--- 5-projects✨/starter/app.js
+++ 5-projects✨/starter/app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.end('welcome!');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'itsyuimoriiTours' });
 });
 
 //Start the server
