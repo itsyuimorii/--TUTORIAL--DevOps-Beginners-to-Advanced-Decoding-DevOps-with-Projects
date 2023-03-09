5841b1241fd66f2287b701cd67798b18a60807b4
YUIMORII <yuimoriistudio@gmail.com>
2023-03-08 23:29:31 -0600

    Update app.js

--- 5-projects✨/starter/app.js
+++ 5-projects✨/starter/app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 //Get an instance (object) of the server
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -13,7 +15,11 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+  // where all the parameters of all the variables that we define here are stored
+  console.log(req.params);
+
+  const tour = tours.find((el) => el.id === req.params.id);
   res.status(200).json({
     //JSend data formations
     status: 'success',
@@ -22,6 +28,31 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //the data from the body in the console, just to verify that it actually works, so req.body.
+  //get data from the body in the console
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  //create a new object by merging two existing objects
+  const newTour = Object.assign({ id: newId }, req.body);
+  //push this tour into the tour array
+  tours.push(newTour);
+  //persist that into file,using fs.writeFileSync
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  //res.send('Done');
 });
 
 //Start the server
