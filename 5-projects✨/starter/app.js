ddfc661a4845fb66c9052e21b0952203bc22e1af
YUIMORII <yuimoriistudio@gmail.com>
2023-03-08 23:30:21 -0600

    Update app.js

--- 5-projects✨/starter/app.js
+++ 5-projects✨/starter/app.js
@@ -19,7 +19,10 @@ app.get('/api/v1/tours/:id', (req, res) => {
   // where all the parameters of all the variables that we define here are stored
   console.log(req.params);
 
+  //So it'll basically loop through the array,and in each of the iterations,we will have access to the current element,and we will return either true or falsein each of the iterations, okay?Now what the find method will then dois that basically, it will create an arraywhich only contains the elementwhere this comparison here turns out to be true, all right?And in this situation, we want to find the elementwhere the ID is equal to the onethat we get from the parameters.And so by specifying this callback function herewith this comparison, we will ensurethat only the element where the ID is actually equalto the specified ID in the parameterswill get returned from the find methodand stored into tour, all right?
+
   const tour = tours.find((el) => el.id === req.params.id);
+
   res.status(200).json({
     //JSend data formations
     status: 'success',
