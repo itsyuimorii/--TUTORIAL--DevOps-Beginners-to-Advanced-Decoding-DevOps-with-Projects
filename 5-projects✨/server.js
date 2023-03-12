8529993a4322ca3aac228b2a243b4cdfc4f1d4f1
YUIMORII <yuimoriistudio@gmail.com>
2023-03-11 20:43:37 -0600

    Update server.js

--- 5-projects✨/server.js
+++ 5-projects✨/server.js
@@ -9,6 +9,12 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 // console.log(app.get('env'));
 console.log(process.env);
 
