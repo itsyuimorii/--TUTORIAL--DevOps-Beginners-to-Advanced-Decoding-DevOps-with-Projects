36d703657ca10fd3c973d66d3c8d36c064399411
YUIMORII <yuimoriistudio@gmail.com>
2023-03-12 18:38:42 -0500

    Update tourModel.js

--- 5-projects✨/models/tourModel.js
+++ 5-projects✨/models/tourModel.js
@@ -32,7 +32,7 @@ const tourSchema = new mongoose.Schema({
     required: [true, `A tour must have a price`],
   },
   priceDiscount: Number,
-  Summary: {
+  summary: {
     type: String,
     trim: true,
     required: [true, `A tour must have a description `],
