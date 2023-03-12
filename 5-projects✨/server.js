67eae15c7d450f367507999c9d4f398336c3f77b
YUIMORII <yuimoriistudio@gmail.com>
2023-03-11 20:41:24 -0600

    UPDATE

--- 5-projects✨/server.js
+++ 5-projects✨/server.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 // console.log(app.get('env'));
 console.log(process.env);
