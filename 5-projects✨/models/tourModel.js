9617f535f1c5b5efe243dd9f6b823f304dd2d034
YUIMORII <yuimoriistudio@gmail.com>
2023-03-12 14:52:44 -0500

    Update tourModel.js

--- 5-projects✨/models/tourModel.js
+++ 5-projects✨/models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, `A tour must have a name`],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, `A tour must have a duration`],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, `A tour must have a max group size`],
+  },
+  difficulty: {
     type: String,
+    required: [true, `A tour must have a difficulty`],
+  },
+  ratingAverage: {
+    type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, `A tour must have a price`],
   },
+  priceDiscount: Number,
+  Summary: {
+    type: String,
+    trim: true,
+    required: [true, `A tour must have a description `],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, `A tour must have a coverImage`],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 /*  
