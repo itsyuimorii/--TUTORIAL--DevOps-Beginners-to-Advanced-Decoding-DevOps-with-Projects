73ebf141aec8b8b7aca6ed496c8874d52fe16b0f
YUIMORII <yuimoriistudio@gmail.com>
2023-03-19 10:25:17 -0500

    Update tourRoutes.js

--- 5-projects✨/Routes/tourRoutes.js
+++ 5-projects✨/Routes/tourRoutes.js
@@ -1,15 +1,13 @@
 const express = require('express');
-
 const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+/* use this .param function here to define parameter middleware in your own applications  */
+// router.param('id', tourController.checkID);
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
-
-/* use this .param function here to define parameter middleware in your own applications  */
-// router.param('id', tourController.checkID);
 
 router.route('/tour-stats').get(tourController.getTourStats);
 
@@ -17,10 +15,11 @@ router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.createTour)
+  .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
 
 module.exports = router;
