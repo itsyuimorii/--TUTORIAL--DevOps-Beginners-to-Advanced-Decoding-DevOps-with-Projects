138cefd5799a55056ccb77853126b70ed1e9193b
YUIMORII <yuimoriistudio@gmail.com>
2023-03-20 15:18:57 -0500

    Update tourModel.js

--- 5-projects✨/models/tourModel.js
+++ 5-projects✨/models/tourModel.js
@@ -73,13 +73,29 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// eslint-disable-next-line prefer-arrow-callback
 tourSchema.pre('save', function (next) {
   console.log('Will save document...');
   next();
 });
 
+// eslint-disable-next-line prefer-arrow-callback
 tourSchema.post('save', function (doc, next) {
   console.log(doc);
+  next();
+});
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
